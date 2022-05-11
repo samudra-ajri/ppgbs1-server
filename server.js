689893bee9c9a19e7b5634d7203d681cab64ae4a
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-05-11 19:19:22 +0700

    Add initial configuration

---
+++ server.js
@@ -0,0 +1,29 @@
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import morgan from 'morgan'
+
+import connectDB from './config/db.js'
+import homeRoutes from './routes/homeRoutes.js'
+
+import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
+
+dotenv.config()
+connectDB()
+const app  = express()
+const PORT = process.env.PORT
+const ENV  = process.env.APP_ENV
+
+if (ENV === 'staging' || ENV === 'local') {
+    app.use(morgan('dev'))
+}
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/', homeRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(PORT, console.log(`Server running in ${ENV} mode on port ${PORT}`))

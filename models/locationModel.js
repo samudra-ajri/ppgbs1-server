e46d17318e64330d69543ce418627e3c8698e107
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-05-28 23:34:36 +0700

    Add uppercas transformation to lacation pre save

--- models/locationModel.js
+++ models/locationModel.js
@@ -13,6 +13,11 @@ const locationSchema = mongoose.Schema({
     timestamps: true
 })
 
+locationSchema.pre('save', async function(next) {
+    this.ds = this.ds.toUpperCase()
+    this.klp = this.klp.map(member => { return member.toUpperCase() })
+})
+
 const Location = mongoose.model('Location', locationSchema)
 
 export default Location

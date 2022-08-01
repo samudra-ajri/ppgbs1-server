e861959901ef48082bb3730a8a430f84c9dce98d
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-08-01 15:52:43 +0700

    Add collation

--- controllers/completionController.js
+++ controllers/completionController.js
@@ -195,6 +195,8 @@ const getAllCompletionsSubjectScores = asyncHandler(async (req, res) => {
             { $match: { $and: filters } },
             { $unwind: "$completed" },
             { $group: { "_id": "$completed", "count": { $sum: 1 } } },
+            { $sort: { _id: 1 } },
+            { $collation: { locale: "en_US", numericOrdering: true } }, // not work for mongo atlass free tier
             {
                 $group: {
                     "_id": null, "targetsCompleted": {

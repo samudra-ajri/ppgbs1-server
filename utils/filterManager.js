978c73aad85506e9a40307c70afc64b6d5ad1226
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-08-10 14:19:20 +0700

    Define more filters

--- utils/filterManager.js
+++ utils/filterManager.js
@@ -2,19 +2,39 @@ import roleTypes from "../consts/roleTypes.js"
 
 // @desc    Filter by manager scope
 const filterManager = (user, search) => {
-    let parameters = [
-        { role: roleTypes.GENERUS },
-        { name: { $regex: new RegExp(search, 'i') } }
-    ]
     switch (user.role) {
         case roleTypes.PPG:
-            return { $and: parameters }
+            return { 
+                $and: [ 
+                    { role: { $in: [roleTypes.GENERUS, roleTypes.MT, roleTypes.MS] }},
+                    { name: { $regex: new RegExp(search, 'i') }}
+                ]
+            }
         case roleTypes.PPD:
-            parameters.push({ ds: user.ds })
-            return { $and: parameters }
+            return { 
+                $and: [ 
+                    { role: { $in: [roleTypes.GENERUS, roleTypes.MT, roleTypes.MS] }},
+                    { name: { $regex: new RegExp(search, 'i') }},
+                    { ds: user.ds }
+                ]
+            }
         case roleTypes.PPK:
-            parameters.push({ klp: user.klp })
-            return { $and: parameters }
+            return { 
+                $and: [ 
+                    { role: { $in: [roleTypes.GENERUS, roleTypes.MT, roleTypes.MS] }},
+                    { name: { $regex: new RegExp(search, 'i') }},
+                    { klp: user.klp }
+                ]
+            }
+        case roleTypes.MT:
+        case roleTypes.MS:
+            return { 
+                $and: [
+                    { role: { $in: [roleTypes.GENERUS] }},
+                    { name: { $regex: new RegExp(search, 'i') }},
+                    { ds: user.ds }
+                ]
+            }
         default:
             return { name: { $regex: new RegExp(search, 'i') }}
     }

a01f5d76f198de8e7b60658f21a2ac25b320225e
Samudra Ajri Kifli <samudra1ajri@gmail.com>
2023-09-30 21:24:36 +0700

    clean code

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,9 +7,9 @@ const router = express.Router()
 router.route('/register').post(authController.register)
 router.route('/login').post(authController.login)
 router.route('/me').get(protect, authController.me)
-router.route('/forgot-password').put(authController.forgotPassword)
+router.route('/forgot-password').post(authController.forgotPassword)
 router.route('/switch-position').post(protect, authController.switchPosition)
-router.route('/temp-password/:token').put(protect, admin, authController.tempPassword)
-router.route('/reset-password/:token').put(authController.resetPassword)
+router.route('/temp-password/:token').post(protect, admin, authController.tempPassword)
+router.route('/reset-password/:token').post(authController.resetPassword)
 
 module.exports = router

468b7d8051a3a16e50912082d8f6fd1c24f37cf0
Samudra Ajri Kifli <samudra1ajri@gmail.com>
2023-12-31 15:14:40 +0700

    feat(auth): exp

--- modules/v1/auth/authController.js
+++ modules/v1/auth/authController.js
@@ -35,7 +35,9 @@ authController.me = asyncHandler(async (req, res) => {
         userId: req.auth.data.id,
         positionId: req.auth.data.position.positionId
     }
+    console.log(req.auth);
     const data = await authService.getUserProfile(payload)
+    data.exp = req.auth.data.exp
     res.json({ data })
     logger({ req, status: loggerStatusConstant.SUCCESS })
 })

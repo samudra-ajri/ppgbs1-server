71564d3b12221f5ef1d9fe66a881d4cc8177aa1a
Samudra Ajri Kifli <samudra1ajri@gmail.com>
2024-02-18 10:25:54 +0700

    fix(completions): optional createdAt

--- modules/v1/completion/completionService.js
+++ modules/v1/completion/completionService.js
@@ -97,7 +97,7 @@ const calculateSumCompletion = (completionsCount, materialsCount, structure, mat
         sumData[structure] = material[structure]
         sumData.materialId = material.id
         sumData.grade = material.grade
-        sumData.createdAt = completion.createdAt
+        sumData.createdAt = completion?.createdAt
         return sumData
     })
 }

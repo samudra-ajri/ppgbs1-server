5a43375aeb3066a958ec7b2d862ec36aeea1c813
Samudra Ajri Kifli <samudra1ajri@gmail.com>
2024-07-27 18:23:00 +0700

    feat(auth): reset password by teacher

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const config = require('../config')
-const { protect, admin, teacher } = require('../middlewares/authMiddleware')
+const { protect, teacher } = require('../middlewares/authMiddleware')
 const userController = require(`../modules/${config.APP_VERSION}/user/userController`)
 const userPositionController = require(`../modules/${config.APP_VERSION}/userPosition/userPositionController`)
 
 const router = express.Router()
 // user
-router.route('/forgot-password').get(protect, admin, userController.forgotPasswordList)
+router.route('/forgot-password').get(protect, teacher, userController.forgotPasswordList)
 router.route('/me').put(protect, userController.updateMyProfile)
 router.route('/me/student').put(protect, userController.updateMyStudentProfile)
 router.route('/me/teacher').put(protect, userController.updateMyTeacherProfile)

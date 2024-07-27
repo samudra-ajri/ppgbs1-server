569bf5fffedc5ac52b437114d6b23bea696fb243
Samudra Ajri Kifli <samudra1ajri@gmail.com>
2024-07-27 17:55:20 +0700

    feat(users): delete by teacher

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const config = require('../config')
-const { protect, admin } = require('../middlewares/authMiddleware')
+const { protect, admin, teacher } = require('../middlewares/authMiddleware')
 const userController = require(`../modules/${config.APP_VERSION}/user/userController`)
 const userPositionController = require(`../modules/${config.APP_VERSION}/userPosition/userPositionController`)
 
@@ -14,7 +14,7 @@ router.route('/').get(protect, userController.list)
 router.route('/download').get(protect, userController.download)
 router.route('/:id').get(protect, userController.detail)
 // user position
-router.route('/:userId/positions/:positionId').delete(protect, admin, userPositionController.delete)
+router.route('/:userId/positions/:positionId').delete(protect, teacher, userPositionController.delete)
 router.route('/positions')
     .post(protect, userPositionController.create)
     .put(protect, userPositionController.change)

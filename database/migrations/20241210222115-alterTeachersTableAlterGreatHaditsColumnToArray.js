57d0dae8d89d91500257cf7a2a8f2cc359fbc3ac
Samudra Ajri Kifli <samudra1ajri@gmail.com>
2026-01-03 22:00:22 +0700

    fix(migrations): Correct SQL syntax for altering greatHadiths column to use ARRAY

--- database/migrations/20241210222115-alterTeachersTableAlterGreatHaditsColumnToArray.js
+++ database/migrations/20241210222115-alterTeachersTableAlterGreatHaditsColumnToArray.js
@@ -1,13 +1,13 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.sequelize.query(`
-      ALTER TABLE "teachers" ALTER COLUMN "greatHadiths" TYPE VARCHAR[];
+      ALTER TABLE "teachers" ALTER COLUMN "greatHadiths" TYPE VARCHAR[] USING ARRAY["greatHadiths"];
     `)
   },
 
   async down(queryInterface, Sequelize) {
     return queryInterface.sequelize.query(`
-      ALTER TABLE "teachers" ALTER COLUMN "greatHadiths" TYPE TEXT; 
+      ALTER TABLE "teachers" ALTER COLUMN "greatHadiths" TYPE TEXT USING ("greatHadiths")[1]; 
     `)
   }
 }
